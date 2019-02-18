db4f0ec87d6658876de47af1d08b34d981dbc2d3
Kevin Nayar <kevin.nayar@gmail.com>
2019-02-18 11:06:02 -0600

    adding timed animation to AgePriceComparison

--- src/components/home/AgePriceComparison.js
+++ src/components/home/AgePriceComparison.js
@@ -1,35 +1,67 @@
 import React from 'react';
 import { object } from 'prop-types';
 
-const AgePriceComparison = React.memo(({ favorites }) => {
-  return (
-    <div className="featured age-price-comparison">
-      <h2>Average price vs. age</h2>
+class AgePriceComparison extends React.Component {
+  constructor(props) {
+    super(props);
+    this.saveRef = React.createRef();
+  }
 
-      {Object.keys(favorites).map(key => (
-        <div className="age-price-comparison-item" key={favorites[key].id}>
-          <h3>{favorites[key].name}</h3>
-          <div className="age-price-comparison-detail">
-            <div className="price-comparison-detail-card">
-              <h4>
-                $
-                {favorites[key].average_price}
-              </h4>
-              <p>Average Price</p>
-            </div>
-            <div className="age-comparison-detail-card">
-              <h4>
-                {favorites[key].average_age}
-                &nbsp;yrs.
-              </h4>
-              <p>Average Age</p>
+  state = {
+    inView: false,
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll = () => {
+    if (this.saveRef.current.getBoundingClientRect().top < window.innerHeight) {
+      this.setState({
+        inView: true,
+      });
+    }
+  }
+
+  render() {
+    const { favorites } = this.props;
+
+    return (
+      <div
+        className={`${this.state.inView ? 'visible' : ''} featured age-price-comparison`}
+        ref={this.saveRef}
+      >
+        <h2>Average price vs. age</h2>
+
+        {Object.keys(favorites).map(key => (
+          <div className="age-price-comparison-item" key={favorites[key].id}>
+            <h3>{favorites[key].name}</h3>
+            <div className="age-price-comparison-detail">
+              <div className="price-comparison-detail-card">
+                <h4>
+                  $
+                  {favorites[key].average_price}
+                </h4>
+                <p>Average Price</p>
+              </div>
+              <div className="age-comparison-detail-card">
+                <h4>
+                  {favorites[key].average_age}
+                  &nbsp;yrs.
+                </h4>
+                <p>Average Age</p>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
-    </div>
-  );
-});
+        ))}
+      </div>
+    );
+  }
+}
 
 AgePriceComparison.propTypes = {
   favorites: object.isRequired,
