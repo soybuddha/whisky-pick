f6233671d2aad0926a3f584565e2b07fd83952e5
Kevin Nayar <kevin.nayar@gmail.com>
2019-02-17 22:30:13 -0600

    making comparison animations responsive

--- src/components/home/RatingComparison.js
+++ src/components/home/RatingComparison.js
@@ -1,28 +1,60 @@
 import React from 'react';
 import { object } from 'prop-types';
 
-const RatingComparison = React.memo(({ favorites }) => {
-  return (
-    <div className="featured rating-comparison">
-      <h2>Average ratings by whisky type</h2>
+class RatingComparison extends React.Component {
+  constructor(props) {
+    super(props);
+    this.saveRef = React.createRef();
+  }
 
-      {Object.keys(favorites).map(key => (
-        <div className="rating-comparison-item" key={favorites[key].id}>
-          <h3>{favorites[key].name}</h3>
-          <div className="comparison-bar">
-            <div className="comparison-outer" style={{ width: '100%' }}>
-              <div className="comparison-inner" style={{ width: `${favorites[key].average_rating}%` }} />
-              <p style={{ left: `${favorites[key].average_rating}%` }}>
-                {favorites[key].average_rating}
-                %
-              </p>
+  state = {
+    inView: false,
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll = () => {
+    if (this.saveRef.current.getBoundingClientRect().top < window.innerHeight) {
+      this.setState({
+        inView: true,
+      });
+    }
+  };
+
+  render() {
+    const { favorites } = this.props;
+
+    return (
+      <div
+        className="featured rating-comparison"
+        ref={this.saveRef}
+      >
+        <h2>Average ratings by whisky type</h2>
+
+        {Object.keys(favorites).map(key => (
+          <div className="rating-comparison-item" key={favorites[key].id}>
+            <h3>{favorites[key].name}</h3>
+            <div className="comparison-bar">
+              <div className={`${this.state.inView ? 'visible' : ''} comparison-outer`} style={{ width: '100%' }}>
+                <div className="comparison-inner" style={{ width: `${favorites[key].average_rating}%` }} />
+                <p style={{ left: `${favorites[key].average_rating}%` }}>
+                  {favorites[key].average_rating}
+                  %
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
-    </div>
-  );
-});
+        ))}
+      </div>
+    );
+  }
+}
 
 RatingComparison.propTypes = {
   favorites: object.isRequired,
